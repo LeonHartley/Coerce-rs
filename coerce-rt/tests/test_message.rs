9cfccf294c77ffd14dab5b72d947c533a82664d0
Leon Hartley <leon.hartley@degree53.com>
2019-12-14 21:02:54 +0000

    tests

--- coerce-rt/tests/test_message.rs
+++ coerce-rt/tests/test_message.rs
@@ -48,52 +48,52 @@ impl Handler<GetCounterRequest> for TestActor {
 
 #[async_trait]
 impl Actor for TestActor {}
-//
-//#[tokio::test]
-//pub async fn test_actor_req_res() {
-//    let mut actor_ref = new_actor(TestActor::new()).await.unwrap();
-//
-//    let response = actor_ref.send(GetStatusRequest()).await;
-//
-//    assert_eq!(response, Ok(GetStatusResponse::None));
-//}
-//
-//#[tokio::test]
-//pub async fn test_actor_req_res_mutation() {
-//    let mut actor_ref = new_actor(TestActor::new()).await.unwrap();
-//
-//    let initial_status = actor_ref.send(GetStatusRequest()).await;
-//    let _ = actor_ref
-//        .send(SetStatusRequest(TestActorStatus::Active))
-//        .await;
-//
-//    let current_status = actor_ref.send(GetStatusRequest()).await;
-//
-//    assert_eq!(initial_status, Ok(GetStatusResponse::None));
-//    assert_eq!(
-//        current_status,
-//        Ok(GetStatusResponse::Ok(TestActorStatus::Active))
-//    );
-//}
-//
-//#[tokio::test]
-//pub async fn test_actor_exec_mutation() {
-//    let mut actor_ref = new_actor(TestActor::new()).await.unwrap();
-//    let initial_status = actor_ref.send(GetStatusRequest()).await;
-//
-//    actor_ref
-//        .exec(|mut actor| {
-//            actor.status = Some(TestActorStatus::Active);
-//        })
-//        .await;
-//
-//    let current_status = actor_ref.send(GetStatusRequest()).await;
-//    assert_eq!(initial_status, Ok(GetStatusResponse::None));
-//    assert_eq!(
-//        current_status,
-//        Ok(GetStatusResponse::Ok(TestActorStatus::Active))
-//    );
-//}
+
+#[tokio::test]
+pub async fn test_actor_req_res() {
+    let mut actor_ref = new_actor(TestActor::new()).await.unwrap();
+
+    let response = actor_ref.send(GetStatusRequest()).await;
+
+    assert_eq!(response, Ok(GetStatusResponse::None));
+}
+
+#[tokio::test]
+pub async fn test_actor_req_res_mutation() {
+    let mut actor_ref = new_actor(TestActor::new()).await.unwrap();
+
+    let initial_status = actor_ref.send(GetStatusRequest()).await;
+    let _ = actor_ref
+        .send(SetStatusRequest(TestActorStatus::Active))
+        .await;
+
+    let current_status = actor_ref.send(GetStatusRequest()).await;
+
+    assert_eq!(initial_status, Ok(GetStatusResponse::None));
+    assert_eq!(
+        current_status,
+        Ok(GetStatusResponse::Ok(TestActorStatus::Active))
+    );
+}
+
+#[tokio::test]
+pub async fn test_actor_exec_mutation() {
+    let mut actor_ref = new_actor(TestActor::new()).await.unwrap();
+    let initial_status = actor_ref.send(GetStatusRequest()).await;
+
+    actor_ref
+        .exec(|mut actor| {
+            actor.status = Some(TestActorStatus::Active);
+        })
+        .await;
+
+    let current_status = actor_ref.send(GetStatusRequest()).await;
+    assert_eq!(initial_status, Ok(GetStatusResponse::None));
+    assert_eq!(
+        current_status,
+        Ok(GetStatusResponse::Ok(TestActorStatus::Active))
+    );
+}
 
 #[tokio::test]
 pub async fn test_actor_exec_chain_mutation() {
@@ -112,17 +112,17 @@ pub async fn test_actor_exec_chain_mutation() {
     let counter = actor_ref.exec(|actor| actor.counter).await;
     assert_eq!(counter, Ok(4));
 }
-//
-//#[tokio::test]
-//pub async fn test_actor_notify() {
-//    let mut actor_ref = new_actor(TestActor::new()).await.unwrap();
-//
-//    for i in 1..=25 as i32 {
-//        let _ = actor_ref
-//            .notify_exec(move |mut actor| actor.counter = i)
-//            .await;
-//    }
-//
-//    let counter = actor_ref.exec(|actor| actor.counter).await;
-//    assert_eq!(counter, Ok(25));
-//}
+
+#[tokio::test]
+pub async fn test_actor_notify() {
+    let mut actor_ref = new_actor(TestActor::new()).await.unwrap();
+
+    for i in 1..=25 as i32 {
+        let _ = actor_ref
+            .notify_exec(move |mut actor| actor.counter = i)
+            .await;
+    }
+
+    let counter = actor_ref.exec(|actor| actor.counter).await;
+    assert_eq!(counter, Ok(25));
+}
