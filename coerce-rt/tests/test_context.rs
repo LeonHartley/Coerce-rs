5796f07a661714dc6d4f8629477ce719d9ed4f8d
Leon Hartley <leon.hartley@degree53.com>
2019-12-16 22:25:04 +0000

    big tests cleanup and more

--- coerce-rt/tests/test_context.rs
+++ coerce-rt/tests/test_context.rs
@@ -1,101 +1,16 @@
-use coerce_rt::actor::context::{ActorContext, ActorHandlerContext};
-use coerce_rt::actor::message::{Exec, Handler, Message, MessageResult};
-use coerce_rt::actor::scheduler::{ActorScheduler, RegisterActor};
-use coerce_rt::actor::{get_actor, new_actor, Actor};
+use util::*;
+use coerce_rt::actor::context::ActorContext;
+
+pub mod util;
+
+#[macro_use]
+extern crate serde;
+extern crate serde_json;
+
+extern crate chrono;
 
 #[macro_use]
 extern crate async_trait;
-
-#[derive(Debug, Eq, PartialEq)]
-pub enum TestActorStatus {
-    Inactive,
-    Active,
-}
-
-#[derive(Debug, Eq, PartialEq)]
-pub struct GetStatusRequest();
-
-#[derive(Debug, Eq, PartialEq)]
-pub enum GetStatusResponse {
-    Ok(TestActorStatus),
-    None,
-}
-
-impl Message for GetStatusRequest {
-    type Result = GetStatusResponse;
-}
-
-#[derive(Debug, Eq, PartialEq)]
-pub struct SetStatusRequest(pub TestActorStatus);
-
-#[derive(Debug, Eq, PartialEq)]
-pub enum SetStatusResponse {
-    Ok,
-    Unsuccessful,
-}
-
-impl Message for SetStatusRequest {
-    type Result = SetStatusResponse;
-}
-
-#[derive(Debug, Eq, PartialEq)]
-pub struct GetCounterRequest();
-
-impl Message for GetCounterRequest {
-    type Result = i32;
-}
-
-pub struct TestActor {
-    pub status: Option<TestActorStatus>,
-    pub counter: i32,
-}
-
-impl TestActor {
-    pub fn new() -> TestActor {
-        TestActor {
-            status: None,
-            counter: 0,
-        }
-    }
-}
-
-#[async_trait]
-impl Handler<GetStatusRequest> for TestActor {
-    async fn handle(
-        &mut self,
-        _message: GetStatusRequest,
-        _ctx: &mut ActorHandlerContext,
-    ) -> GetStatusResponse {
-        match self.status {
-            Some(TestActorStatus::Active) => GetStatusResponse::Ok(TestActorStatus::Active),
-            Some(TestActorStatus::Inactive) => GetStatusResponse::Ok(TestActorStatus::Inactive),
-            _ => GetStatusResponse::None,
-        }
-    }
-}
-
-#[async_trait]
-impl Handler<SetStatusRequest> for TestActor {
-    async fn handle(
-        &mut self,
-        message: SetStatusRequest,
-        _ctx: &mut ActorHandlerContext,
-    ) -> SetStatusResponse {
-        self.status = Some(message.0);
-
-        SetStatusResponse::Ok
-    }
-}
-
-#[async_trait]
-impl Handler<GetCounterRequest> for TestActor {
-    async fn handle(&mut self, _message: GetCounterRequest, _ctx: &mut ActorHandlerContext) -> i32 {
-        self.counter
-    }
-}
-
-#[async_trait]
-impl Actor for TestActor {}
 
 #[tokio::test]
 pub async fn test_context_get_actor() {
--- coerce-rt/tests/util/mod.rs
+++ coerce-rt/tests/util/mod.rs
@@ -1,7 +1,120 @@
 use chrono::Local;
+use coerce_rt::actor::context::{ActorContext, ActorHandlerContext};
+use coerce_rt::actor::message::{Handler, Message};
+use coerce_rt::actor::{new_actor, Actor, ActorRef};
+use coerce_rt::remote::context::RemoteActorContext;
+use coerce_rt::remote::handler::RemoteActorMessageMarker;
+use coerce_rt::remote::*;
 use env_logger::Builder;
 use log::LevelFilter;
 use std::io::Write;
+use std::mem::forget;
+use std::time::Duration;
+
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+pub enum TestActorStatus {
+    Inactive,
+    Active,
+}
+
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+pub struct GetStatusRequest();
+
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+pub enum GetStatusResponse {
+    Ok(TestActorStatus),
+    None,
+}
+
+impl Message for GetStatusRequest {
+    type Result = GetStatusResponse;
+}
+
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+pub struct SetStatusRequest {
+    pub status: TestActorStatus,
+}
+
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+pub enum SetStatusResponse {
+    Ok,
+    Unsuccessful,
+}
+
+impl Message for SetStatusRequest {
+    type Result = SetStatusResponse;
+}
+
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+pub struct GetCounterRequest();
+
+impl Message for GetCounterRequest {
+    type Result = i32;
+}
+
+pub struct TestActor {
+    pub status: Option<TestActorStatus>,
+    pub counter: i32,
+}
+
+impl TestActor {
+    pub fn new() -> TestActor {
+        TestActor {
+            status: None,
+            counter: 0,
+        }
+    }
+}
+
+#[async_trait]
+impl Handler<GetStatusRequest> for TestActor {
+    async fn handle(
+        &mut self,
+        _message: GetStatusRequest,
+        _ctx: &mut ActorHandlerContext,
+    ) -> GetStatusResponse {
+        match self.status {
+            Some(TestActorStatus::Active) => GetStatusResponse::Ok(TestActorStatus::Active),
+            Some(TestActorStatus::Inactive) => GetStatusResponse::Ok(TestActorStatus::Inactive),
+            _ => GetStatusResponse::None,
+        }
+    }
+}
+
+#[async_trait]
+impl Handler<SetStatusRequest> for TestActor {
+    async fn handle(
+        &mut self,
+        message: SetStatusRequest,
+        _ctx: &mut ActorHandlerContext,
+    ) -> SetStatusResponse {
+        self.status = Some(message.status);
+
+        SetStatusResponse::Ok
+    }
+}
+
+#[async_trait]
+impl Handler<GetCounterRequest> for TestActor {
+    async fn handle(&mut self, _message: GetCounterRequest, _ctx: &mut ActorHandlerContext) -> i32 {
+        self.counter
+    }
+}
+
+#[async_trait]
+impl Actor for TestActor {}
+
+pub struct EchoActor {}
+
+#[async_trait]
+impl Actor for EchoActor {}
+
+#[async_trait]
+impl Handler<GetCounterRequest> for EchoActor {
+    async fn handle(&mut self, _message: GetCounterRequest, _ctx: &mut ActorHandlerContext) -> i32 {
+        42
+    }
+}
 
 pub fn create_trace_logger() {
     Builder::new()
