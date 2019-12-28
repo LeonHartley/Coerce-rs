a9f8d42d2d976e1fa2b28a26779dd6f20fca7ca7
Leon Hartley <leon.hartley@degree53.com>
2019-12-28 18:45:12 +0000

    pluggable worker store
    coming soon: mysql worker store

--- coerce-remote/src/cluster/workers.rs
+++ coerce-remote/src/cluster/workers.rs
@@ -1,20 +1,84 @@
 use crate::cluster::node::RemoteNode;
 use chrono::{DateTime, Utc};
+use coerce_rt::actor::context::ActorHandlerContext;
+use coerce_rt::actor::message::{Handler, Message};
+use coerce_rt::actor::Actor;
 use std::error::Error;
 use std::time::Instant;
 use uuid::Uuid;
 
+#[derive(Debug)]
 pub enum WorkerStoreErr {}
 
 #[async_trait]
 pub trait WorkerStore {
-    async fn get_active() -> Result<Vec<ClusterWorker>, WorkerStoreErr>;
+    async fn get_active(&mut self) -> Result<Vec<ClusterWorker>, WorkerStoreErr>;
 
-    async fn update(worker: &ClusterWorker) -> Result<(), WorkerStoreErr>;
+    async fn update(&mut self, worker: &ClusterWorker) -> Result<(), WorkerStoreErr>;
+}
+
+pub struct ClusterWorkers {
+    store: Box<dyn WorkerStore + Send + Sync>,
+}
+
+impl ClusterWorkers {
+    pub fn new<T: WorkerStore>(store: T) -> ClusterWorkers
+    where
+        T: 'static + Sync + Send,
+    {
+        let store = Box::new(store);
+        ClusterWorkers { store }
+    }
+}
+
+impl Actor for ClusterWorkers {}
+
+pub struct GetActiveWorkers;
+
+impl Message for GetActiveWorkers {
+    type Result = Result<Vec<ClusterWorker>, WorkerStoreErr>;
+}
+
+pub struct UpdateWorker(ClusterWorker);
+
+impl Message for UpdateWorker {
+    type Result = Result<(), WorkerStoreErr>;
+}
+
+#[async_trait]
+impl Handler<GetActiveWorkers> for ClusterWorkers {
+    async fn handle(
+        &mut self,
+        message: GetActiveWorkers,
+        ctx: &mut ActorHandlerContext,
+    ) -> Result<Vec<ClusterWorker>, WorkerStoreErr> {
+        self.store.get_active().await
+    }
+}
+
+#[async_trait]
+impl Handler<UpdateWorker> for ClusterWorkers {
+    async fn handle(
+        &mut self,
+        message: UpdateWorker,
+        ctx: &mut ActorHandlerContext,
+    ) -> Result<(), WorkerStoreErr> {
+        self.store.update(&message.0).await
+    }
 }
 
 pub struct ClusterWorker {
     id: Uuid,
     node: RemoteNode,
-    last_ping: DateTime<Utc>,
+    last_ping: Option<DateTime<Utc>>,
+}
+
+impl ClusterWorker {
+    pub fn new(id: Uuid, node: RemoteNode, last_ping: Option<DateTime<Utc>>) -> ClusterWorker {
+        ClusterWorker {
+            id,
+            node,
+            last_ping,
+        }
+    }
 }
---
+++ coerce-remote/tests/test_remote_cluster_workers.rs
@@ -0,0 +1,47 @@
+#[macro_use]
+extern crate serde;
+extern crate serde_json;
+
+extern crate chrono;
+
+#[macro_use]
+extern crate async_trait;
+
+use coerce_remote::cluster::node::RemoteNode;
+use coerce_remote::cluster::workers::{ClusterWorker, ClusterWorkers, WorkerStore, WorkerStoreErr, GetActiveWorkers};
+use coerce_rt::actor::context::ActorContext;
+use uuid::Uuid;
+
+pub mod util;
+
+pub struct TestWorkerStore;
+
+#[async_trait]
+impl WorkerStore for TestWorkerStore {
+    async fn get_active(&mut self) -> Result<Vec<ClusterWorker>, WorkerStoreErr> {
+        let node_1 = Uuid::new_v4();
+        let node_2 = Uuid::new_v4();
+
+        Ok(vec![
+            ClusterWorker::new(node_1, RemoteNode::new(node_1, "127.0.0.1", 1024), None),
+            ClusterWorker::new(node_2, RemoteNode::new(node_2, "127.0.0.1", 1025), None),
+        ])
+    }
+
+    async fn update(&mut self, worker: &ClusterWorker) -> Result<(), WorkerStoreErr> {
+        Ok(())
+    }
+}
+
+#[tokio::test]
+pub async fn test_remote_worker_store() {
+    let mut context = ActorContext::new();
+    let mut workers = context
+        .new_anon_actor(ClusterWorkers::new(TestWorkerStore))
+        .await
+        .unwrap();
+
+    let active_workers = workers.send(GetActiveWorkers).await.unwrap().unwrap();
+
+    assert_eq!(active_workers.len(), 2);
+}
