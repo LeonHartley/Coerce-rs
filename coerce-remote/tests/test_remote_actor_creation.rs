e75e438c95403d8bd3c1959df4d4f2f72b764701
Leon Hartley <leon.hartley@degree53.com>
2020-04-11 17:59:21 +0100

    test for handling a `CreateActor` remote message

--- coerce-remote/tests/test_remote_actor_creation.rs
+++ coerce-remote/tests/test_remote_actor_creation.rs
@@ -13,7 +13,12 @@ use coerce_remote::storage::activator::ActorActivator;
 use coerce_remote::storage::state::{ActorStore, ActorStoreErr};
 use coerce_rt::actor::{Actor, ActorId, ActorState};
 
+use coerce_remote::context::RemoteActorContext;
+use coerce_rt::actor::context::ActorContext;
 use uuid::Uuid;
+use coerce_remote::net::message::{CreateActor, ActorCreated};
+use coerce_remote::codec::json::JsonCodec;
+use coerce_remote::codec::MessageCodec;
 
 pub struct TestActorStore {
     state: Option<ActorState>,
@@ -30,9 +35,31 @@ impl Actor for TestActor {}
 pub async fn test_remote_actor_create_new() {
     util::create_trace_logger();
 
+    let actor_type = "TestActor";
     let actor_id = format!("{}", Uuid::new_v4());
     let expected_actor_name = "test-actor".to_string();
     let actor_state = format!("{{\"name\": \"{}\"}}", &expected_actor_name).into_bytes();
+
+    let codec = JsonCodec::new();
+    let mut context = ActorContext::new();
+    let mut remote = RemoteActorContext::builder()
+        .with_actor_context(context)
+        .with_actors(|builder| builder.with_actor::<TestActor>("TestActor"))
+        .build()
+        .await;
+
+    let message = CreateActor {
+        id: Uuid::new_v4(),
+        actor_id: Some(actor_id.clone()),
+        actor_type: actor_type.to_string(),
+        actor: actor_state
+    };
+
+    let result = remote.create_actor(message).await;
+    let create_actor_res = codec.decode_msg::<ActorCreated>(result.unwrap()).unwrap();
+
+    assert_eq!(create_actor_res.id, actor_id);
+    assert_eq!(create_actor_res.node_id, remote.node_id());
 }
 
 #[tokio::test]
