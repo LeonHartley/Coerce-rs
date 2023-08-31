fdc525160b35ed0a0394069207123c57c84b90e5
Leon Hartley <leon.hartley@sharpgaming.com>
2023-08-31 12:42:15 +0100

    fix new networking metrics labels (src_addr and dest_addr)

--- coerce/src/remote/net/metrics.rs
+++ coerce/src/remote/net/metrics.rs
@@ -2,6 +2,7 @@ pub const METRIC_NETWORK_BYTES_RECV: &str = "coerce_network_bytes_recv";
 pub const METRIC_NETWORK_BYTES_SENT: &str = "coerce_network_bytes_sent";
 
 pub const LABEL_SRC_ADDR: &str = "src_addr";
+pub const LABEL_DEST_ADDR: &str = "dest_addr";
 
 pub struct NetworkMetrics;
 
@@ -10,7 +11,7 @@ impl NetworkMetrics {
     pub fn incr_bytes_received(len: u64, src_addr: &str) {
         #[cfg(feature = "metrics")]
         increment_counter!(METRIC_NETWORK_BYTES_RECV,
-            LABEL_SRC_ADDR => len.to_string()
+            LABEL_SRC_ADDR => src_addr
         );
     }
 
@@ -18,7 +19,7 @@ impl NetworkMetrics {
     pub fn incr_bytes_sent(len: u64, dest_addr: &str) {
         #[cfg(feature = "metrics")]
         increment_counter!(METRIC_NETWORK_BYTES_SENT,
-            LABEL_SRC_ADDR => len.to_string()
+            LABEL_DEST_ADDR => dest_addr
         );
     }
 }
